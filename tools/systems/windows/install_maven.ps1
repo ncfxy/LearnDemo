04c2c97b628ec0f3c27d2919f1be6608dab53840
ncfxy <359419163@qq.com>
2020-02-11 12:05:44 +0800

    Update install_maven.ps1

--- tools/systems/windows/install_maven.ps1
+++ tools/systems/windows/install_maven.ps1
@@ -8,8 +8,8 @@ $ncfxyInstallDir = [Environment]::GetEnvironmentVariable('ncfxyInstallDir','User
 if (![System.IO.Directory]::Exists($ncfxyDownLoadDir)) {[void][System.IO.Directory]::CreateDirectory($ncfxyDownLoadDir)}
 if (![System.IO.Directory]::Exists($ncfxyInstallDir)) {[void][System.IO.Directory]::CreateDirectory($ncfxyInstallDir)}
 
-$url = 'https://mirrors.tuna.tsinghua.edu.cn/apache/maven/maven-3/3.6.1/binaries/apache-maven-3.6.1-bin.zip'
-$downloadFilePath = $ncfxyDownLoadDir + 'apache-maven-3.6.1-bin.zip'
+$url = 'https://mirrors.tuna.tsinghua.edu.cn/apache/maven/maven-3/3.5.4/binaries/apache-maven-3.5.4-bin.zip'
+$downloadFilePath = $ncfxyDownLoadDir + 'apache-maven-3.5.4-bin.zip'
 downloadFile $url $downloadFilePath
 $targetPath = $ncfxyInstallDir + 'maven/'
 $targetArgument = '-o' + $targetPath
@@ -19,6 +19,6 @@ $path7za = $ncfxyDownLoadDir + '7za.exe'
 downloadFile $url7za $path7za
 $process = Start-Process $path7za -ArgumentList 'x',$downloadFilePath,$targetArgument,'-y' -PassThru
 $process.WaitForExit()
-"Finish installing apache-maven-3.6.1" 
+"Finish installing apache-maven-3.5.4" 
 
 
